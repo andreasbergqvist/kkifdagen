2fa1f8d63d02dc948e75945fb0d085846666eaeb
Andreas Berqvist <andreas@itiden.se>
2026-05-14 19:39:45 +0200

    Fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import { BrowserRouter } from "react-router-dom";
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import './index.css';
 
-const routerBase = import.meta.env.BASE_URL.endsWith("/")
+const routerBase = import.meta.env.BASE_URL.endsWith('/')
   ? import.meta.env.BASE_URL.slice(0, -1)
   : import.meta.env.BASE_URL;
 
-createRoot(document.getElementById("root")).render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter basename={routerBase}>
     <App />
   </BrowserRouter>,
